6e84dd0758211dcf23d9fa017fb45e13d1e0e281
Andrea Satria Nagari <93112763+GrmRy@users.noreply.github.com>
2023-08-17 04:15:29 +0000

    add function to get a data

--- hello_world.py
+++ hello_world.py
@@ -3,9 +3,12 @@ from flask import Flask
 app= Flask(__name__)
 
 @app.route("/")
-
 def helloworld():
     return "Hello World"
+
+@app.route("/get_data")
+def getdata():
+    return "Your data"
 
 if __name__=="__main__":
     app.run()
